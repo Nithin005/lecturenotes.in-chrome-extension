e3426c70d8e5be5ea0f08d42c5d6ccbaa1c09133
vj-abishek <abigosearch@gmail.com>
2020-10-18 12:44:55 +0530

    UI Changes and some bug fix

--- injectScript.js
+++ injectScript.js
@@ -1,58 +1,68 @@
-let navBar;
 let doc;
 let i = 0;
 let reader;
 let pageNodes;
-let observer = new MutationObserver(main);
+const MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+const observer = new MutationObserver(main);
 const config = { attributes: true, childList: true, subtree: true };
-let button;
 
-
-
+// A helper to listen for params change
+(function (history) {
+    var pushState = history.pushState;
+    history.pushState = function (state) {
+        if (typeof history.onpushstate == "function") {
+            history.onpushstate({ state: state });
+        }
+        return pushState.apply(history, arguments);
+    };
+})(window.history);
 
 
 function constructButton() {
-    navBar = document.querySelector("body > div.MuiDialog-root.reader-track > div.MuiDialog-container.MuiDialog-scrollPaper > div > div > div > div > header.MuiPaper-root.MuiAppBar-root.MuiAppBar-positionAbsolute.MuiAppBar-colorInherit.MuiPaper-elevation4 > div");
+    const { search } = window.location;
+    let insterted = false;
 
-    button = document.createElement('button');
-    button.setAttribute('class', 'dl-button');
-    button.value = 'Download';
-    button.innerText = "Download"
-    button.onclick = buttonClicked;
-    let buttonDiv = document.createElement('div');
-    buttonDiv.className = 'MuiButtonBase-root';
-    buttonDiv.appendChild(button);
-    navBar.insertBefore(buttonDiv, navBar.children[2]);
+    if (!insterted) {
+        insterted = true;
+        const dom = document.querySelector('body');
+        const button = document.createElement('button');
+        button.setAttribute('class', 'dl-button');
+        button.innerHTML = `
+        <span class="MuiButton-startIcon MuiButton-iconSizeMedium"><svg class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H8V4h12v12zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm12 6V9c0-.55-.45-1-1-1h-2v5h2c.55 0 1-.45 1-1zm-2-3h1v3h-1V9zm4 2h1v-1h-1V9h1V8h-2v5h1zm-8 0h1c.55 0 1-.45 1-1V9c0-.55-.45-1-1-1H9v5h1v-2zm0-2h1v1h-1V9z"></path></svg></span>
+        <div>Download PDF</div>
+        `;
+        button.onclick = buttonClicked;
+        dom.appendChild(button);
+    }
 
-
+    if (search !== '?reading=true') {
+        document.querySelector('.dl-button').style.display = 'flex';
+    } else {
+        document.querySelector('.dl-button').style.display = 'none';
+    }
 }
 
 function buttonClicked() {
-    let = script = document.createElement('script');
+    const script = document.createElement('script');
+
     document.body.appendChild(script);
-    
-    script.onload = function(){
-        setTimeout(()=>
-        {
-            
-            reader = document.querySelector("body > div.MuiDialog-root.reader-track > div.MuiDialog-container.MuiDialog-scrollPaper > div > div > div > div > main");
+
+    script.onload = function () {
+        setTimeout(() => {
+            reader = document.querySelector("main");
             pageNodes = reader.querySelector("div").children;
-            reader.scroll(0,0);
+            reader.scroll(0, 0);
             main();
             randomScroll();
-        },1000);
-      
-
+        }, 1000);
     };
     script.src = 'https://unpkg.com/jspdf@latest/dist/jspdf.umd.min.js';
-
-   
 }
 
 
 function main() {
-
     let imgElement = pageNodes[i].querySelector("div.MuiPaper-root > div > img");
+    const button = document.querySelector('.dl-button');
     if (imgElement === null) {
         pageNodes[i].scrollIntoView(true);
         observer.observe(pageNodes[i], config);
@@ -60,35 +70,39 @@ function main() {
     }
     observer.disconnect();
     let url = imgElement.src;
-    if(i===0){
-        doc = jspdf.jsPDF('p','px',[imgElement.naturalWidth,imgElement.naturalHeight]);
+    if (i === 0) {
+        doc = jspdf.jsPDF('p', 'px', [imgElement.naturalWidth, imgElement.naturalHeight]);
     }
     let imgProp = doc.getImageProperties(url);
-    doc.addImage(imgProp.data, 'JPEG', 0, 0,imgProp.width,imgProp.height);
-    console.log(imgElement.naturalWidth,imgElement.naturalHeight)
-    button.innerText = (i+1)+'/'+pageNodes.length;
+    doc.addImage(imgProp.data, 'JPEG', 0, 0, imgProp.width, imgProp.height);
+    console.log(imgElement.naturalWidth, imgElement.naturalHeight)
+    button.innerText = (i + 1) + '/' + pageNodes.length
     console.log(url);
     console.log(i);
     i++;
-    if (i >= pageNodes.length-1) {
-        setTimeout(()=>{
-            i=0;
-        },5000);
+    if (i >= pageNodes.length - 1) {
+        setTimeout(() => {
+            i = 0;
+        }, 5000);
         let titleElement = document.querySelector("head > title");
         doc.save(titleElement.innerText);
         button.innerText = 'Download';
         return;
     }
+    console.log(i);
     doc.addPage();
     main();
 }
 
 function randomScroll() {
-    let scrollLen = -window.innerHeight;
-    reader.scrollBy(0, scrollLen);
-    if (i < pageNodes.length - 1) {
-        setTimeout(() => { randomScroll() }, 3000);
-    }
+    const nextButton = document.querySelector("body > div.MuiDialog-root.reader-track > div.MuiDialog-container.MuiDialog-scrollPaper > div > div > div > div > header.MuiPaper-root.MuiAppBar-root.MuiAppBar-positionFixed.MuiAppBar-colorPrimary.jss461.mui-fixed.MuiPaper-elevation4 > div > button:nth-child(6) > span.MuiIconButton-label")
+    setInterval(() => {
+        nextButton.click();
+        nextButton.click();
+    }, 300);
 }
 
-constructButton();
+window.onpopstate = history.onpushstate = () => {
+    console.log('State Changed');
+    constructButton();
+};
