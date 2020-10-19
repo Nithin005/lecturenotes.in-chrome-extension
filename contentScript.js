91466519f6d17efc0e64e2855d644c8f7beecf25
vj-abishek <abigosearch@gmail.com>
2020-10-19 16:46:45 +0530

    Linking local js file instead of remote file

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,8 @@
 window.onload = () => {
-    let script = document.createElement('script');
-    script.src = chrome.runtime.getURL('injectScript.js')
-    document.head.appendChild(script);
+    let script1 = document.createElement('script');
+    script1.src = chrome.runtime.getURL('injectScript.js')
+    document.head.appendChild(script1);
+    let script2 = document.createElement('script');
+    script2.src = chrome.runtime.getURL('./lib/jspdf.umd.min.js');
+    document.head.appendChild(script2);
 }
