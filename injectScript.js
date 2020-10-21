8fde06f938d70b92ccdb22352ad5a785023a90f8
Nithin005 <mcleaannithin@gmail.com>
2020-10-21 12:06:39 +0530

    refractored code & changed readme

--- injectScript.js
+++ injectScript.js
@@ -5,57 +5,7 @@ let pageNodes;
 const MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
 const observer = new MutationObserver(main);
 const config = { attributes: true, childList: true, subtree: true };
-
-// A helper to listen for params change
-(function (history) {
-    var pushState = history.pushState;
-    history.pushState = function (state) {
-        if (typeof history.onpushstate == "function") {
-            history.onpushstate({ state: state });
-        }
-        return pushState.apply(history, arguments);
-    };
-})(window.history);
-
-
-function constructButton() {
-    const { search } = window.location;
-    let insterted = false;
-
-    if (!insterted) {
-        insterted = true;
-        const dom = document.querySelector('body');
-        const button = document.createElement('button');
-        button.setAttribute('class', 'dl-button');
-        button.setAttribute('type', 'button');
-        button.innerHTML = `
-        <div>Download PDF</div>
-        `;
-        button.onclick = buttonClicked;
-        dom.appendChild(button);
-    }
-
-    if (search !== '?reading=true') {
-        document.querySelector('.dl-button').style.display = 'flex';
-    } else {
-        document.querySelector('.dl-button').style.display = 'none';
-    }
-}
-
-function buttonClicked() {
-    const button = document.querySelector('.dl-button');
-    i = 0;
-
-    button.innerText = 'Loading please wait...';
-    setTimeout(() => {
-        reader = document.querySelector("main");
-        pageNodes = reader.querySelector("div").children;
-        reader.scroll(0, 0);
-        main();
-        randomScroll();
-    }, 1000);
-}
-
+let inserted = false;
 
 function main() {
     let imgElement = pageNodes[i].querySelector("div.MuiPaper-root > div > img");
@@ -72,12 +22,12 @@ function main() {
     }
     let imgProp = doc.getImageProperties(url);
     doc.addImage(imgProp.data, 'JPEG', 0, 0, imgProp.width, imgProp.height);
-    button.innerText = `${i + 1} page loaded out of ${pageNodes.length - 1}`;
+    button.innerText = `${i + 1} pages loaded out of ${pageNodes.length - 1}`;
     i++;
     if (i >= pageNodes.length - 1) {
         let titleElement = document.querySelector("head > title");
         doc.save(titleElement.innerText);
-        button.innerText = 'Download';
+
         return;
     }
     doc.addPage();
@@ -97,10 +47,73 @@ function randomScroll() {
                 <div style="margin-left:9px">Downloaded</div>
             </div>
             `
+            setTimeout(()=>{
+                button.innerText = 'Download';
+            },3000)
         }
-    }, 1000);
+    }, 1000)
+}
+
+function buttonClicked() {
+    const button = document.querySelector('.dl-button');
+    i = 0;
+
+    button.innerText = 'Loading please wait...';
+    //setTimeout(() => {
+    reader = document.querySelector("main");
+    pageNodes = reader.querySelector("div").children;
+    reader.scroll(0, 0);
+    main();
+    randomScroll();
+    // }, 1000);
+}
+
+function constructButton() {
+
+
+
+    inserted = true;
+    const dom = document.querySelector('body');
+    const button = document.createElement('button');
+    //clean this
+    button.style.display = "flex";
+    button.setAttribute('class', 'dl-button');
+    button.innerHTML = `
+        <div>Download PDF</div>
+        `;
+    button.onclick = buttonClicked;
+    dom.appendChild(button);
+
+
+}
+
+// A helper to listen for params change
+(function (history) {
+    var pushState = history.pushState;
+    history.pushState = function (state) {
+        if (typeof history.onpushstate == "function") {
+            history.onpushstate({ state: state });
+        }
+        return pushState.apply(history, arguments);
+    };
+})(window.history);
+
+if ((/\breading=true\b/.test(location.search))) {
+    // GM_log("triggered")
+    constructButton();
 }
 
 window.onpopstate = history.onpushstate = () => {
-    constructButton();
-};
+    if (!(/\breading=true\b/.test(location.search))) {
+        if (!inserted) {
+            constructButton();
+        }
+        document.querySelector('.dl-button').style.display = 'flex';
+        //GM_log("triggered")
+    }
+    else {
+        if (inserted) {
+            document.querySelector('.dl-button').style.display = 'none';
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -4,10 +4,11 @@
 <h1 align="center">Lecturenotes downloader</h1>
 
 <p align="center">
-    <img src="./assets/1.jpg" width="500" />
+    <img src="./assets/1.png" width="500" />
 </p>
 
 ### `Steps`
 1. Go to [lecturenotes.in](https://lecturenotes.in/) and choose your notes
 1. Then click on `read now` button in lecturesnotes.in webpage.
 1. Then the chrome extension will be automatically loaded. 
+1. Hit <img src="./assets/2.png"/> button
